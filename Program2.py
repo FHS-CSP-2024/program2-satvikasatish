8ee21f15d4124433fe6c36e2ddfce229ad390958
satvikasatish <157325905+satvikasatish@users.noreply.github.com>
2024-09-04 15:10:59 +0000

    I finished program 2 assignment

--- Program2.py
+++ Program2.py
@@ -50,7 +50,9 @@
 #Please write a script that: 
 # - Asks for the user's name and then prints it twice, on two consecutive lines.
 
-
+name=input("What's your name?")
+print(name)
+print(name)
 
 
 
@@ -59,7 +61,8 @@
 # - Asks for the user's name
 # - Prints it out twice on a single line so that there is an exclamation mark at the beginning of the line, 
 # - another between the two names and a third one at the end of the line.
-
+name=input("What's your name?")
+print("!" + name + "!" + name + "!")
 
 ## Problem 3 ##
 #Please write a script that: 
@@ -70,13 +73,22 @@
 #   - Last name: Sanders
 #   - Street address: 91 Station Road
 #   - City and postal code: Folsom CA, 95630
-
+name = input("Give me your first name.")
+lastname = input("Give me your surname.")
+address = input("What's your street address?")
+city = input("Name your city and postal code.")
+print("First name: " + name)
+print("Last name: " + lastname)
+print("Street address: " + address)
+print("City and postal code: " + city)
 ## Problem 4 ##
 #Please write a script that: 
 # - Asks for 3 words 
 # - Puts the words together with dashes and prints that out
-
-
+noun = input("Give me a noun.")
+adj = input("Give me an adjective.")
+verb = input("Give me a verb.")
+print(noun+"_"+verb+"_"+adj)
 ## Problem 5 ##
 #Please write a script that: 
 # - Asks for a name and a year
@@ -90,4 +102,4 @@
 #Only Mary could save the village's residents.
 name = input("Give me a girl's name.")
 year = input("What year were you born?")
-print("When" + name "woke up in the morning, she found out that she had sprouted wings." )
+print ("When " + name + " woke up in the morning, she found out that she had traveled back in time several years. It was now " + year " and she had to fix all her mistakes before the present." )
